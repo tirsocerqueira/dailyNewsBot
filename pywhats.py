eda6feabf925e2dab94cac8bd8dd98ea4e85e081
tirsocerqueira <tcerque@gmail.com>
2022-11-06 16:39:43 +0100

    Receiving and sending it to WhatsApp

--- pywhats.py
+++ pywhats.py
@@ -1,3 +1,19 @@
 import pywhatkit as kit
+import requests
 
-kit.sendwhatmsg("+34656366579","Hello World",13,16)
+#Retrieving data from New York Times
+res = requests.get("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=zwEN89uPuhAG8Ysrrb7D18xjuRHwAkSL")
+registros = res.json()['results']
+
+msg=""
+
+for i in registros:
+    msg+=i['title']
+    msg+='\n'
+    msg+=(i['url'])
+    msg+='\n'
+
+print(msg)
+
+#Sending data to Suscribers
+kit.sendwhatmsg("+34656366579",msg,16,33)
