cf4d29d429484a7b69e1a6ebfa75acee43294d08
tirsocerqueira <tcerque@gmail.com>
2022-11-10 12:30:56 +0100

    Sending message at hour of execution and minute plus 2.

--- pywhats.py
+++ pywhats.py
@@ -1,14 +1,10 @@
-import time
 import pywhatkit as kit
 import requests
-import pyautogui
-import keyboard as k
 from datetime import datetime
 
 #Retrieving data from New York Times
 res = requests.get("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=zwEN89uPuhAG8Ysrrb7D18xjuRHwAkSL")
 registros = res.json()['results']
-
 #Establish number on the number constant
 msg=""
 
@@ -22,11 +18,6 @@ for i in registros:
 currentDateAndTime = datetime.now()
 hour=currentDateAndTime.strftime("%H")
 minute=currentDateAndTime.strftime("%M")
-kit.sendwhatmsg_instantly("+34656366579",msg)
-time.sleep(2)
-pyautogui.click()
-time.sleep(1)
-k.press_and_release('enter')
-
-
-
+hour=int(hour)
+minute=int(minute)+2
+kit.sendwhatmsg("",msg,hour,minute,15)
