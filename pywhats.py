8f8d29f8c8eef16dfdce48ed5e2368399d8268fc
tirsocerqueira <tcerque@gmail.com>
2022-11-10 11:50:48 +0100

    Receiving and sending it to WhatsApp

--- pywhats.py
+++ pywhats.py
@@ -1,10 +1,15 @@
+import time
 import pywhatkit as kit
 import requests
+import pyautogui
+import keyboard as k
+from datetime import datetime
 
 #Retrieving data from New York Times
 res = requests.get("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=zwEN89uPuhAG8Ysrrb7D18xjuRHwAkSL")
 registros = res.json()['results']
 
+#Establish number on the number constant
 msg=""
 
 for i in registros:
@@ -13,7 +18,15 @@ for i in registros:
     msg+=(i['url'])
     msg+='\n'
 
-print(msg)
-
 #Sending data to Suscribers
-kit.sendwhatmsg("+34656366579",msg,16,33)
+currentDateAndTime = datetime.now()
+hour=currentDateAndTime.strftime("%H")
+minute=currentDateAndTime.strftime("%M")
+kit.sendwhatmsg_instantly("+34656366579",msg)
+time.sleep(2)
+pyautogui.click()
+time.sleep(1)
+k.press_and_release('enter')
+
+
+
